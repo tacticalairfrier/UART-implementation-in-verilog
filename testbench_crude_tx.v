5b39d8bc859861badc41f6a9f4b7ee0070c916b0
Chinmay Tupkari <ctupkari5@gmail.com>
2026-04-06 20:58:18 +0530

    rx is bug free with parity bit included

--- testbench_crude_tx.v
+++ testbench_crude_tx.v
@@ -6,19 +6,20 @@ module testbench;
 reg [7:0] data_in_tb;
 reg clkgen, reset, TX_enable_tb;
 wire data_out_tb;
-//debug ports
-wire debug_baud_tick_tb;
-wire [1:0] debug_state_tb, debug_nextstate_tb;
+// //debug ports
+// wire [2:0] debug_state_tb, debug_nextstate_tb;
+// wire debug_baud_tick_tb, debug_parity_tb;
 UART_tx DUT(
     .data_in(data_in_tb),
     .clkin(clkgen),
     .reset(reset),
     .TX_enable(TX_enable_tb),
-    .data_out(data_out_tb),
-    //debug ports
-    .debug_baud(debug_baud_tick_tb),
-    .debug_state(debug_state_tb),
-    .debug_nextstate(debug_nextstate_tb)
+    .data_out(data_out_tb)
+    // //debug ports
+    // .debug_baud(debug_baud_tick_tb),
+    // .debug_state(debug_state_tb),
+    // .debug_nextstate(debug_nextstate_tb),
+    // .debug_parity(debug_parity_tb)
 );
 
 initial begin 
@@ -35,7 +36,7 @@ initial begin
     $dumpfile("sim.vcd");
     $dumpvars(0,testbench);
     TX_enable_tb = 1'b1;
-    data_in_tb = 8'b01100011;
+    data_in_tb = 8'b11111111;
     $monitor($time, "at this time the value of out register is %b", data_out_tb);
     #100000;
     TX_enable_tb = 1'b0;
